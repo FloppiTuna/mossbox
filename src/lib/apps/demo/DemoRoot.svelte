064dd963892432d699171d3cf65e283b8f617a28
Griffin <griffin@edforma.app>
2026-08-15 23:04:27 -0500

    list component wow

--- src/lib/apps/demo/DemoRoot.svelte
+++ src/lib/apps/demo/DemoRoot.svelte
@@ -1,6 +1,8 @@
 <script>
     import MDProgressBar from "$lib/components/MBProgressBar.svelte";
     import MDDialogBox from "$lib/components/MBDialogBox.svelte";
+    import MBList from "$lib/components/MBList.svelte";
+    import Cursor20Filled from "virtual:icons/fluent/cursor-20-filled";
 </script>
 
 <main class="demo-root">
@@ -12,4 +14,10 @@
     ]}>
         <p>This is a dialog box. All changes will be lost. Spooky!</p>
     </MDDialogBox>
+
+    <MBList items={[
+        { label: "Item 1", icon: Cursor20Filled, onClick: () => console.log("Item 1 clicked") },
+        { label: "Item 2", icon: Cursor20Filled, onClick: () => console.log("Item 2 clicked") },
+        { label: "Item 3", icon: Cursor20Filled, inactive: true, onClick: () => console.log("Item 3 clicked")}
+    ]} />
 </main>
---
+++ src/lib/components/MBList.svelte
@@ -0,0 +1,96 @@
+<script lang="ts">
+    let { items } = $props();
+</script>
+
+<main class="list">
+    <ul class="nav">
+        {#each items as item}
+            <li>
+                <button
+                    type="button"
+                    class="row-content row-button"
+                    class:inactive={item.inactive}
+                    onclick={() => item.onClick()}
+                >
+                    <div class="list-icon">
+                        {#if item.icon}
+                            <item.icon />
+                        {/if}
+                    </div>
+
+                    <div class="list-label">{item.label}</div>
+                </button>
+            </li>
+        {/each}
+    </ul>
+</main>
+
+<style>
+    .list .nav {
+        min-width: 0;
+
+        background: #000000;
+        box-sizing: border-box;
+    }
+
+    .list ul {
+        list-style: none;
+        padding: 0;
+        margin: 0;
+    }
+
+    .list .nav li {
+        padding: 0;
+        border-bottom: 1px solid #2a2a2a;
+        height: 42px;
+    }
+
+    .list .nav li:hover {
+        background: #2f2238;
+    }
+
+    .list .nav .row-content {
+        display: flex;
+        align-items: center;
+        height: 100%;
+        width: 100%;
+        box-sizing: border-box;
+        padding: 0 1rem;
+        line-height: 1.25;
+    }
+
+    .list .nav .row-content.inactive {
+        opacity: 0.5;
+    }
+
+    .list .nav .row-button {
+        text-align: left;
+        background: transparent;
+        color: inherit;
+        border: 0;
+        margin: 0;
+        cursor: pointer;
+        font: inherit;
+        line-height: inherit;
+        appearance: none;
+    }
+
+    /* .list .nav li.active {
+        background: #49315a;
+    } */
+
+    .list .nav .list-icon {
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        width: 1.5rem;
+        height: 1.5rem;
+        margin-right: 0.6rem;
+        flex: 0 0 auto;
+    }
+
+    .list .nav .list-icon :global(svg) {
+        width: 100%;
+        height: 100%;
+    }
+</style>
