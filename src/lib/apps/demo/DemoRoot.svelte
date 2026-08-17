7142688b539140fa59efd0433ce5645dcfd76977
Griffin Bauer <griffinrbauer@icloud.com>
2026-08-16 21:35:13 -0500

    i maded a burton :D

--- src/lib/apps/demo/DemoRoot.svelte
+++ src/lib/apps/demo/DemoRoot.svelte
@@ -2,15 +2,19 @@
     import MDProgressBar from "$lib/components/MBProgressBar.svelte";
     import MDDialogBox from "$lib/components/MBDialogBox.svelte";
     import MBList from "$lib/components/MBList.svelte";
+    import MBButton from "$lib/components/MBButton.svelte";
+
     import Cursor20Filled from "virtual:icons/fluent/cursor-20-filled";
 
     import { playUISound } from "$lib/sfx";
-
+    
+    let demoProgress = $state(50);
 </script>
 
 <main class="demo-root">
-    <p>TESTTEST</p>
-    <MDProgressBar progress={100} />
+    <p>Progress Bar</p>
+    <MDProgressBar progress={demoProgress} />
+    <MBButton label="Randomize progress" icon={Cursor20Filled} onClick={() => { demoProgress = Math.floor(Math.random() * 101); }} />
     <!-- <MDDialogBox actions={[
         { label: "OK", onClick: () => console.log("OK clicked") },
         { label: "Cancel", onClick: () => console.log("Cancel clicked") }
@@ -23,4 +27,5 @@
         { label: "* Listey the List!", icon: Cursor20Filled, onClick: () => { }},
         { label: "You are filled with LISTIFICATION.", icon: Cursor20Filled, inactive: true, onClick: () => { } }
     ]} />
+
 </main>
---
+++ src/lib/components/MBButton.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+    import { playUISound } from "$lib/sfx";
+    import type { Component } from "svelte";
+
+    let { label, icon, inactive, onClick } = $props<{
+        label: string;
+        icon?: Component;
+        inactive?: boolean;
+        onClick?: () => void;
+    }>();
+</script>
+<!-- naming is kind of ambiguous oops -->
+<main class="button"> 
+    <button class:inactive={inactive} onmouseenter={() => playUISound("NAVIGATE")} onclick={() => { onClick?.(); playUISound("SELECT"); }}>
+        {#if icon}
+            <div class="list-icon">
+                <icon></icon>
+            </div>
+        {/if}
+        <span class="label">{label}</span>
+    </button>
+</main>
+
+<style>
+    .button {
+        display: flex;
+    }
+
+    button {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        padding: 0.6rem 1.0rem;
+
+        border: none;
+        background-color: #000000;
+        color: #ffffff;
+
+        font-size: 0.9rem;
+        font-weight: 500;
+
+        cursor: pointer;
+
+        /* transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out; */
+    }
+
+    button:hover {
+        background-color: #2f2238;
+    }
+
+    button:active {
+        background-color: #49315a;
+    }
+
+    button.inactive {
+        opacity: 0.5;
+        cursor: not-allowed;
+    }
+</style>
